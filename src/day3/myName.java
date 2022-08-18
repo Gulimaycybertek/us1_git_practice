6015667e9d3464c35b6595ee759dbb20bdb89d52
gulimayurazbayeva <77guli@mail.ru>
2022-08-18 07:53:57 -0600

    This is my first name

---
+++ src/day3/myName.java
@@ -0,0 +1,11 @@
+package day3;
+
+public class myName {
+
+    public static void main(String[] args) {
+
+        System.out.println("My name is Gulimay");
+
+        // next line fill last name
+    }
+}
