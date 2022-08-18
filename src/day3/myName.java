31685e20478dcb73e39f8bb765bafcae7452661a
gulimayurazbayeva <77guli@mail.ru>
2022-08-18 07:55:09 -0600

    This is my last name

--- src/day3/myName.java
+++ src/day3/myName.java
@@ -7,5 +7,9 @@ public class myName {
         System.out.println("My name is Gulimay");
 
         // next line fill last name
+
+        //Here my last name
+
+        System.out.println("My last name is Urazbayeva");
     }
 }
